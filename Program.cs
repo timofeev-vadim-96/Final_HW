e569edf01024c46901e9da3fb044f4381ea5c8d5
Vadim Timofeev <timofeev.vadim.96@mail.ru>
2023-01-27 20:22:41 +0300

    Метод для подсчета кол-ва слов, длина которых менее 4

--- Program.cs
+++ Program.cs
@@ -4,3 +4,16 @@
 // ["hello"], "2", "world", ":-)"]->["2",":-)"]
 // ["1,2,3,4"], "1567", "-2", "computer science"]->["-2"]
 // ["Russia", "Denmark", "Kazan"] - > []
+
+string [] textArray = {"hello", "2", "world", ":-)"};
+int quantityLessThen4 = 0; //кол-во слов, длина которых меньше 4 символов
+void HowManyWordsIsLessThen4 (string [] startArray)
+{
+    for (int i = 0; i < textArray.Length; i++)
+    {
+        if (textArray[i].Length <= 3)
+        {
+            quantityLessThen4++;
+        }
+    }
+}
